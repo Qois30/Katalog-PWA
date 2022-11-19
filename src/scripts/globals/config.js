b161c811f3fb5a6054fc160097e6c04e038d523a
Qois Al 'Ariq <78062644+Qois30@users.noreply.github.com>
2022-11-19 22:33:21 +0700

    Master

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '69c1bf4fd3fa3f2b549f951882d1fd5f',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us'
+}
+
+export default CONFIG
