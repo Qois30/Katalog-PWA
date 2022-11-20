d46ec7ad7a45a516e8389a9d3f9facc26676b9bd
Qois Al 'Ariq <78062644+Qois30@users.noreply.github.com>
2022-11-20 22:41:42 +0700

    Update Function Favorite/Like
    Clear function Favorite

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,7 +2,11 @@ const CONFIG = {
   KEY: 'YOUR_API_KEY',
   BASE_URL: 'https://restaurant-api.dicoding.dev',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
-  DEFAULT_LANGUAGE: 'en-us'
+  DEFAULT_LANGUAGE: 'en-us',
+
+  DATABASE_NAME: 'Restaurant-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'restaurants'
 }
 
 export default CONFIG
